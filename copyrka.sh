20fe8229e19b1f21e5e9874a08d488c0fdf7ebe7
fedor123123 <fedor.palilov@mail.ru>
2024-05-02 17:03:52 +0300

    Добавлен Bash-скрипт

---
+++ copyrka.sh
@@ -0,0 +1,23 @@
+#!/bin/bas
+
+#принимаем два файла
+input_dir=$1
+output_dir=$2
+
+#Получаем список файлов во входной директории
+files=$(find "$input_dir" -maxdepth 1 -type f)
+echo "$files"
+
+#Получаем список директорий во входной директории
+dirs=$(find "$input_dir" -maxdepth 1 -type d)
+echo "$dirs"
+
+# Получение списка всех файлов
+allfiles=$(find "$input_dir" -type f)
+echo "$allfiles"
+
+# Копирование файлов
+echo "$allfiles" | while read file; do
+    cp "$file" "$output_dir"
+done
+
